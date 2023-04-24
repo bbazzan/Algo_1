b11e4bb478c5f67eacc5592b7d0375959e2249fc
bbazzan <brunobazzan@gmail.com>
2023-04-24 19:34:23 -0300

    ejercicio 2

--- guia_5.hs
+++ guia_5.hs
@@ -1,4 +1,11 @@
+import Distribution.Simple.Utils (xargs)
 -- Ej 1
 longitud :: [t] -> Integer
 longitud [] = 0
 longitud (_:xs) = 1 + longitud xs
+
+-- Ej 2
+ultimo :: [t] -> t
+ultimo [x] = x
+ultimo (_:xs) = ultimo xs
+
