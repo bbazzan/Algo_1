5f837d054e1fdd538e6c2f9e7de013aa13669809
bbazzan <brunobazzan@gmail.com>
2023-04-25 17:51:45 -0300

    agrega archivo de la guia 1 y el ejercicio 2 de la guia 5

--- guia_5.hs
+++ guia_5.hs
@@ -19,3 +19,58 @@ principio (x:xs) = x : principio xs
 reverso :: [t] -> [t]
 reverso [] = []
 reverso (x:xs) = reverso xs ++ [x]
+
+-- Ej 2
+-- a
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece y (x:xs) 
+    | y == x = True
+    | otherwise = pertenece y xs
+
+-- b
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = True
+todosIguales [_] = True
+todosIguales (x:y:xs) = x == y && todosIguales (y:xs)
+
+-- c
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos (x:xs) = not (pertenece x xs) && todosDistintos xs
+
+-- d
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) = pertenece x xs || hayRepetidos xs
+
+-- e
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar y (x:xs) 
+    | y == x = xs
+    | otherwise = x : quitar y xs
+
+-- f
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos y (x:xs) 
+    | y == x = quitarTodos y xs
+    | otherwise = x : quitarTodos y xs
+
+-- g
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) = x : eliminarRepetidos (quitarTodos x xs)
+
+-- h
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos xs ys = todosPertenecen (eliminarRepetidos xs) (eliminarRepetidos ys) && todosPertenecen (eliminarRepetidos ys) (eliminarRepetidos xs)
+
+todosPertenecen :: (Eq t) => [t] -> [t] -> Bool
+todosPertenecen [] _ = True
+todosPertenecen (x:xs) ys = pertenece x ys && todosPertenecen xs ys
+
+-- i
+capicua :: (Eq t) => [t] -> Bool
+capicua xs = xs == reverso xs
