936abdcc152dcbaf783a4a119ea921f835b84afb
Bruno Bazzan <chronos@MacBook-Pro-de-appdirect.local>
2023-06-08 15:01:46 -0300

    ejercicio 7

--- guia_10.py
+++ guia_10.py
@@ -79,4 +79,70 @@ def invertirArchivo(nombre_archivo: str) -> None:
     
     archivo.close()
     
-invertirArchivo("test.txt")
+# invertirArchivo("test.txt")
+
+# Ej 4
+# funcion que dado un archivo y una frase (que puede estar separada por '\n' y/o ' '), 
+# devuelve el archivo con la frase agregada al final (sin hacer un clon)
+
+def agregarFrase(nombre_archivo: str, frase: str) -> None:
+    archivo: str = open(nombre_archivo, 'a')
+    archivo.write(frase)
+    archivo.close()
+    
+# agregarFrase("test.txt", "Hola mundo \nHola galaxia \nHola universo")
+
+# Ej 5
+# lo mismo que antes pero agregando la frase al principio
+
+def agregarFraseAlPrincipio(nombre_archivo: str, frase: str) -> None:
+    archivo: str = open(nombre_archivo, 'r')
+    lineas: list[str] = archivo.readlines()
+    archivo.close()
+    
+    archivo: str = open(nombre_archivo, 'w')
+    archivo.write(frase)
+    
+    for linea in lineas:
+        archivo.write(linea)
+    
+    archivo.close()
+
+# agregarFraseAlPrincipio("test.txt", "Hola ciudad \nHola pais \nHola mundo \n")
+
+# Ej 6
+# funcion que lee un archivo en binario y devuelve una lista de palabras legibles, donde un plabra legible es
+# aquella formada por numero letras mayusculas y/o minusculas y los caracteres esapcio y guion bajo y ademas
+# tiene un longitud mayor o igual a 5 caracteres
+
+
+# Ej 7
+# funcio que lee un archivo .csv y calcula el promedio de las notas de los alumnos. El archivo tiene la siguiente
+# estructura: lu (str), materia (str), fecha (str), nota (float)
+
+def promedioEstudiante(lu: str):
+    archivo: str = open("notas.csv", 'r')
+    lineas: list[str] = archivo.readlines()
+    archivo.close()
+    
+    # print(lineas)
+    
+    notas: list[float] = []
+    
+    for linea in lineas:
+        datos: list[str] = linea.split(",")
+        # print(datos)
+        if datos[0] == lu:
+            notas.append(float(datos[3]))
+            
+    # print(notas)
+    
+    suma: float = 0
+    
+    for nota in notas:
+        suma += nota
+    
+    return suma / len(notas)
+
+# print(promedioEstudiante("448/17"))
+
